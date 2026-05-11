52dbafe25c8507c1c21d3e1ae14de5544dd7cb95
Adriano Fernandes Braz BRAZ <u07994189199@acad.pp.ifms.edu.br>
2026-05-11 13:36:00 -0400

    Mais um teste no computador do if

---
+++ src/bean/AfbUsuarios.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author u07994189199
+ */
+public class AfbUsuarios {
+
+    /**
+     * @return the afbIdUsuarios
+     */
+    public int getAfbIdUsuarios() {
+        return afbIdUsuarios;
+    }
+
+    /**
+     * @param afbIdUsuarios the afbIdUsuarios to set
+     */
+    public void setAfbIdUsuarios(int afbIdUsuarios) {
+        this.afbIdUsuarios = afbIdUsuarios;
+    }
+
+    /**
+     * @return the afbNome
+     */
+    public String getAfbNome() {
+        return afbNome;
+    }
+
+    /**
+     * @param afbNome the afbNome to set
+     */
+    public void setAfbNome(String afbNome) {
+        this.afbNome = afbNome;
+    }
+
+    /**
+     * @return the afbApelido
+     */
+    public String getAfbApelido() {
+        return afbApelido;
+    }
+
+    /**
+     * @param afbApelido the afbApelido to set
+     */
+    public void setAfbApelido(String afbApelido) {
+        this.afbApelido = afbApelido;
+    }
+
+    /**
+     * @return the afbCpf
+     */
+    public String getAfbCpf() {
+        return afbCpf;
+    }
+
+    /**
+     * @param afbCpf the afbCpf to set
+     */
+    public void setAfbCpf(String afbCpf) {
+        this.afbCpf = afbCpf;
+    }
+
+    /**
+     * @return the afbDataNascimento
+     */
+    public Date getAfbDataNascimento() {
+        return afbDataNascimento;
+    }
+
+    /**
+     * @param afbDataNascimento the afbDataNascimento to set
+     */
+    public void setAfbDataNascimento(Date afbDataNascimento) {
+        this.afbDataNascimento = afbDataNascimento;
+    }
+
+    /**
+     * @return the afbSenha
+     */
+    public String getAfbSenha() {
+        return afbSenha;
+    }
+
+    /**
+     * @param afbSenha the afbSenha to set
+     */
+    public void setAfbSenha(String afbSenha) {
+        this.afbSenha = afbSenha;
+    }
+
+    /**
+     * @return the afbNivel
+     */
+    public int getAfbNivel() {
+        return afbNivel;
+    }
+
+    /**
+     * @param afbNivel the afbNivel to set
+     */
+    public void setAfbNivel(int afbNivel) {
+        this.afbNivel = afbNivel;
+    }
+
+    /**
+     * @return the afbAtivo
+     */
+    public String getAfbAtivo() {
+        return afbAtivo;
+    }
+
+    /**
+     * @param afbAtivo the afbAtivo to set
+     */
+    public void setAfbAtivo(String afbAtivo) {
+        this.afbAtivo = afbAtivo;
+    }
+    private int afbIdUsuarios;
+    private String afbNome;
+    private String afbApelido;
+    private String afbCpf;
+    private Date afbDataNascimento;
+    private String afbSenha;
+    private int afbNivel;
+    private String afbAtivo;
+}
---
+++ src/dao/DaoAfbUsuarios.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+
+import bean.AfbUsuarios;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author u07994189199
+ */
+
+public class DaoAfbUsuarios  extends DaoAfbAbstract {
+     @Override
+    public void insert(Object object) {
+        AfbUsuarios afbUsuarios = (AfbUsuarios) object;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url, user, password;
+            url = "jdbc:mysql://10.7.0.51:33062/db_adriano_braz";
+            user = "adriano_braz";
+            password = "adriano_braz";
+            Connection cnt;
+            cnt = DriverManager.getConnection(url, user, password);
+            String sql = "insert into Afb_usuarios values (?,?,?,?,?,?,?,?)";
+            PreparedStatement pst = cnt.prepareStatement( sql );
+            pst.setInt(1, afbUsuarios.getAfbIdUsuarios());
+            pst.setString(2, afbUsuarios.getAfbNome());
+            pst.setString(3, afbUsuarios.getAfbApelido());
+            pst.setString(4, afbUsuarios.getAfbCpf());
+            pst.setDate(5, null);
+            pst.setInt(6, afbUsuarios.getAfbNivel());
+            pst.setString(7, afbUsuarios.getAfbSenha());
+            pst.setString(8, afbUsuarios.getAfbAtivo());
+            pst.executeUpdate();
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DaoAfbUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoAfbUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
